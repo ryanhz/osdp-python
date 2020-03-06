55afb2352294e94a815b3a023f65e69662b2c63e
Ryan Hu <huzhiren@gmail.com>
2020-03-06 14:58:41 +1300

    Release v1.0

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@ from distutils.core import setup
 setup(
 	name = 'osdp',
 	packages = ['osdp'],
-	version = '0.5',
+	version = '1.0',
 	license='apache-2.0',
 	description = 'A Python control panel implementation of the Open Supervised Device Protocol (OSDP)',
 	long_description = open('README.rst').read(),
 	author = 'Ryan Hu',
 	author_email = 'huzhiren@gmail.com',
 	url = 'https://github.com/ryanhz/osdp-python',
-	download_url = 'https://github.com/ryanhz/osdp-python/archive/v0.5-alpha.tar.gz',
+	download_url = 'https://github.com/ryanhz/osdp-python/archive/v1.0.tar.gz',
 	keywords = ['OSDP', 'Open Supervised Device Protocol', 'Access Control'],
 	install_requires=[
 		'pycryptodome',
 		'pyserial >= 3.4',
 	],
 	classifiers=[
-		'Development Status :: 3 - Alpha',
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: Apache Software License',
 		'Topic :: Communications',
